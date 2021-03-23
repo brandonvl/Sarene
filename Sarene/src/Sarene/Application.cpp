9c43ee64404bd0d7e39b91e89f46e2ee8f7dd234
brandonvl <6137891+brandonvl@users.noreply.github.com>
2021-03-23 22:27:00 +0100

    Initial project setup

---
+++ Sarene/src/Sarene/Application.cpp
@@ -0,0 +1,28 @@
+#include "Application.h"
+
+namespace Sarene
+{
+	Application::Application()
+	{
+
+	}
+
+	Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+}
+
+
+// Fine for now, rework later
+int main()
+{
+	Sarene::Application *app = new Sarene::Application();
+	app->Run();
+	delete app;
+}
---
+++ Sarene/src/Sarene/Application.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Sarene
+{
+	class Application
+	{
+		public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+}
+
