e416cb4250eae555f992f035639c1f86d322e693
brandonvl <6137891+brandonvl@users.noreply.github.com>
2021-03-25 22:28:29 +0100

    Add premake configuration

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+workspace "Sarene"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sarene"
+	location "Sarene"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+
+	filter "configurations:Debug"
+		defines "SAR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SAR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SAR_DIST"
+		optimize "On"
