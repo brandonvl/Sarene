6b7ee9f324a7bd0ec586ffbd62074db6501a7b8e
brandonvl <6137891+brandonvl@users.noreply.github.com>
2021-09-12 22:30:05 +0200

    Add GLFW input polling

--- Sarene/src/Platform/Windows/WindowsWindow.h
+++ Sarene/src/Platform/Windows/WindowsWindow.h
@@ -32,6 +32,11 @@ namespace Sarene
 		void SetVSync(bool enabled);
 		bool IsVSync() const;
 
+		inline virtual void* GetNativeWindow() const
+		{
+			return m_Window;
+		}
+
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
