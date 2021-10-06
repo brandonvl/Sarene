e38814c432a80b80fc5dc025fba13339d197ea5f
brandonvl <6137891+brandonvl@users.noreply.github.com>
2021-10-06 22:45:56 +0200

    Create buffer abstraction for  Vertex & Index buffer

--- Sarene/src/Sarene/Application.h
+++ Sarene/src/Sarene/Application.h
@@ -10,6 +10,7 @@
 #include "Sarene/ImGui/ImGuiLayer.h"
 
 #include "Sarene/Renderer/Shader.h"
+#include "Sarene/Renderer/Buffer.h"
 
 namespace Sarene
 {
@@ -43,8 +44,10 @@ namespace Sarene
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 
 		static Application* s_Instance;
 	};
---
+++ Sarene/src/Sarene/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "sarpch.h"
+#include "Renderer.h"
+
+namespace Sarene
+{
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
